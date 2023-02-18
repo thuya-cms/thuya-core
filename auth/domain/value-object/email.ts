637c7f903c1f22bc6aab7129e5cbde84699166cb
Geri101 <s.geri92@gmail.com>
2023-02-18 23:03:49 +0100

    Create user entity

---
+++ auth/domain/value-object/email.ts
@@ -0,0 +1,38 @@
+import IdentifiableError from "../../../framework/identitfiable-error";
+
+enum ErrorCode {
+    Invalid = "invalid"
+}
+
+class Email {
+    /**
+     * Email value object.
+     * 
+     * @param email the email
+     * @throws will throw an error if the email format is not valid
+     */
+    constructor(private email: string) {
+        if (!this.isEmailValid(email))
+            throw new IdentifiableError(ErrorCode.Invalid, "Email address is invalid.");
+    }
+
+
+
+    value(): string {
+        return this.email;
+    }
+
+    equals(email: Email): boolean {
+        return email.value() === this.email;
+    }
+
+
+    private isEmailValid(email: string) {
+        const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        
+        return regex.test(email);
+    };
+}
+
+export default Email;
+export { ErrorCode };
---
+++ auth/domain/value-object/password.ts
@@ -0,0 +1,57 @@
+import bcrypt from 'bcrypt';
+import IdentifiableError from '../../../framework/identitfiable-error';
+
+enum ErrorCode {
+    Invalid = "invalid"
+}
+
+class Password {
+    private hashedPassword: string;
+
+
+
+    /**
+     * Password value object storing it in hashed format.
+     * 
+     * @param password the password in hashed or not hashed format
+     * @param isHashed true if the password is already hashed
+     * @throws will throw an error when the password format is not correct
+     */
+    constructor(password: string, isHashed: boolean = false) {
+        if (!isHashed && !this.isPasswordValid(password))
+            throw new IdentifiableError(ErrorCode.Invalid, "Password format is invalid.");
+
+        if (isHashed)
+            this.hashedPassword = password;
+        else
+            this.hashedPassword = bcrypt.hashSync(password, 8);
+    }
+
+ 
+
+    static empty(): Password {
+        return new Password("", true);
+    }
+
+
+    value(): string {
+        return this.hashedPassword;
+    }
+
+    compare(password: string): boolean {
+        return bcrypt.compareSync(password, this.hashedPassword);
+    }
+
+    isEmpty(): boolean {
+        return this.hashedPassword.trim() === "";
+    }
+
+
+    private isPasswordValid(password: string): boolean {
+        const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,20}$/;
+
+        return regex.test(password);
+    }
+}
+
+export default Password;
