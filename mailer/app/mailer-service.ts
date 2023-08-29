cb7624795f032a50f6075434207bbafb04837ed0
Geri101 <s.geri92@gmail.com>
2023-08-29 22:01:05 +0200

    Implement mail service for HTML content

--- mailer/app/mailer-service.ts
+++ mailer/app/mailer-service.ts
@@ -1,3 +1,4 @@
+import sendHtmlMail from "../domain/usecase/send-html-mail";
 import sendMail from "../domain/usecase/send-mail";
 
 /**
@@ -5,17 +6,35 @@ import sendMail from "../domain/usecase/send-mail";
  */
 class MailerService {
     /**
-     * Send an email.
+     * Send an email with text content.
      * 
      * @param from the sender of the email
      * @param to the target of the email
      * @param subject the subject of the email
      * @param body the content of the email
+     * @param options additional options
+     * @param options.attachments attachments for the email
      * @async
      */
-    async sendMail(from: string, to: string[], subject: string, body: string): Promise<void> {
+    async sendMail(from: string, to: string[], subject: string, body: string, options?: { attachments?: { filename: string, path: string, cid: string }[] }): Promise<void> {
         await sendMail.init();
-        sendMail.execute(from, to, subject, body);
+        sendMail.execute(from, to, subject, body, options);
+    }
+
+    /**
+     * Send an email with HTML content.
+     * 
+     * @param from the sender of the email
+     * @param to the target of the email
+     * @param subject the subject of the email
+     * @param body HTML content of the email
+     * @param options additional options
+     * @param options.attachments attachments for the email
+     * @async
+     */
+    async sendHTMLMail(from: string, to: string[], subject: string, body: string, options?: { attachments?: { filename: string, path: string, cid: string }[] }): Promise<void> {
+        await sendHtmlMail.init();
+        sendHtmlMail.execute(from, to, subject, body, options);
     }
 }
 
